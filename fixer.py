2dd10c5924668dbe93ef01484c7b1a0ee8f333c1
Juanjo <juanjocatalagarrido@gmail.com>
2021-08-06 19:26:52 +0200

    Added script

---
+++ fixer.py
@@ -0,0 +1,107 @@
+#! /usr/bin/python3
+#
+# Coded by --> JCGdev
+#
+# Convert your CSV file to a valid JSON for NewPipe  
+#
+#
+
+import os
+import sys
+import json
+import csv
+
+runtimePath: str = os.getcwd()
+jsonHeader: dict = {"app_version":"0.21.7","app_version_int":973,"subscriptions":[]}
+
+
+
+
+def getFilenamesInDir() -> tuple:
+    return os.listdir()
+
+
+def getCsvFilename(filenames: str) -> str:
+    return "".join(name for name in filenames if name.endswith(".csv"))
+
+def getFileDescriptor(filename: str) -> None:
+    return open(filename)
+
+def readFileLines(filename: str) -> dict:
+    with open(filename, "r") as file:
+        return file.readlines()
+
+def readFile(filename: str) -> str:
+    with open(filename, "r") as file:
+        return file.read()
+
+def writeLinesToFile(filename: str, content: list) -> None:
+    with open(filename, "w") as file:
+        file.writelines(content)
+
+def writeToFile(filename: str, content: str):
+    with open(filename, "w") as file:
+        file.write(content)
+
+
+# ---------- Main functions --------
+
+
+def prepareCsv(filename: str) -> list:
+
+    debugPrint("Parsing CSV file...")
+
+    csvContent: list = readFileLines(filename)
+    csvContent[0] = "service_id,url,name\n" # Overriding not working key names from the 1st line
+
+    parsedCsvContent: list = []
+
+    for line in csvContent:
+        parsedCsvContent.append(line.replace("http", "https")) 
+
+    return parsedCsvContent
+
+
+def convertCsvToJson(filename: str) -> str:
+
+    debugPrint("Converting parsed CSV to JSON file...")    
+
+    global jsonHeader
+
+    csvConverted: list = jsonHeader 
+    csvParser = csv.DictReader(getFileDescriptor(filename))
+    
+    for row in csvParser:
+        csvConverted["subscriptions"].append(row)
+        
+    debugPrint(f"Done! file saves as 'subscriptions.json'")
+
+    return json.dumps(csvConverted, indent=4)
+
+
+
+def debugPrint(message: str) -> None:
+    print(f"[*] {message} \n")
+
+
+
+def main():
+
+    filenames: list = getFilenamesInDir()
+    csvFilename: str = getCsvFilename(filenames)
+
+    debugPrint(f"Detected CSV File --> {csvFilename}")
+
+    parsedCsvContent: list = prepareCsv(csvFilename)
+    writeLinesToFile("parsed_subscriptions.csv", parsedCsvContent)
+
+    convertedCsvToJson: str = convertCsvToJson("parsed_subscriptions.csv")
+    writeToFile("subscriptions.json", convertedCsvToJson)
+
+    
+    
+     
+
+
+if __name__ == "__main__":
+    main()
